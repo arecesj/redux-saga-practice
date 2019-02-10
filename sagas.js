3fc13f7cc3862d2a268b106081b2756368541804
JUAN ARECES <arecesj@gmail.com>
2019-02-10 13:06:04 -0800

    added single test. wrapped up redux saga tutorial

--- sagas.js
+++ sagas.js
@@ -1,4 +1,4 @@
-import { put, takeEvery, all } from 'redux-saga/effects';
+import { put, takeEvery, all, call } from 'redux-saga/effects';
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
@@ -8,7 +8,7 @@ export function* helloSaga() {
 
 // a worker saga. AKA will perform the async task
 export function* incrementAsync() {
-  yield delay(1000);
+  yield call(delay, 1000);
   yield put({ type: 'INCREMENT' });
 }
 
